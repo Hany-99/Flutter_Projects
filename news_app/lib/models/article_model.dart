39225324a7ff600c1b82784d2b662f9fb98bd6aa
Hany-99 <hanyhazzem404@gmail.com>
2024-10-24 14:56:18 +0300

    Delete news_app/lib directory

--- news_app/lib/models/article_model.dart
+++
@@ -1,8 +0,0 @@
-class ArticleModel {
-  final String? image;
-  final String title;
-  final String? subTitle;
-
-  ArticleModel(
-      {required this.image, required this.title, required this.subTitle});
-}
